c03c3111830ac41120b5bf93da69c143ca4b942a
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-27 19:11:48 +0300

    Handle errors from response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,12 +11,11 @@ def number_of_subscribers(subreddit):
     Returns total subscribers for a given subreddit,
     0 if subreddit is invalid
     """
-    if not subreddit or type(subreddit) is not str:
-        return 0
-
     url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16-api_advanced:v1.0.0 (by /u/Sn0wF1re)"}
     response = requests.get(url, headers=headers)
+    if response == 404:
+        return 0
     results = response.json().get("data")
     subs = results.get("subscribers")
     return subs
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,10 +11,6 @@ def top_ten(subreddit):
     """
     prints titles of first 10 hot posts listed for a given subreddit
     """
-    if not subreddit or type(subreddit) is not str:
-        print("None")
-        return
-
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16-api_advanced:v1.0.0 (by /u/Sn0wF1re)"
@@ -22,5 +18,9 @@ def top_ten(subreddit):
     params = {"limit": 10}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
+    
+    if response == 404:
+        print("None")
+        return
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
