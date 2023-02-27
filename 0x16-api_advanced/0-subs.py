474d5e6b2a72f1724a8a4d04c0174661e1468cf6
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-27 17:06:05 +0300

    Write script to retrieve number of subscribers for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Contains function to return number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns total subscribers for a given subreddit,
+    0 if subreddit is invalid
+    """
+    if not subreddit or type(subreddit) is not str:
+        return 0
+
+    url = f"http://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "linux:0x16-api_advanced:v1.0.0 (by /u/Sn0wF1re)"}
+    response = requests.get(url, headers=headers)
+    results = response.json().get("data")
+    subs = results.get("subscribers")
+    return subs
